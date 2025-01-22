7a942f2019dfa01861d1430710c39a3e028e4356
hamdy haggag <hamdyhaggag74@gmail.com>
2025-01-22 15:58:51 +0200

    feat: add custom primary color palette for UI enhancements

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,9 +7,17 @@ abstract class AppColors {
   static const Color blackColor = Colors.black;
   static Color greyColor = Colors.grey;
   static const customColors = [
-    Color(0xFF6200EA),
-    Color(0xFF03DAC5),
-    Color(0xFFBB86FC),
-    Color(0xFF3700B3),
+    Color(0xFF0961F5), // Primary Color
+    Color(0xFF3381F7), // 90% Tint
+    Color(0xFF66A2F9), // 80% Tint
+    Color(0xFF99C2FB), // 70% Tint
+    Color(0xFFCCE3FD), // 60% Tint
+    Color(0xFF084CD3), // 10% Shade
+    Color(0xFF063EAF), // 20% Shade
+    Color(0xFF052F8B), // 30% Shade
+    Color(0xFF041F66), // 40% Shade
+    Color(0xFF3979C8), // Muted Cool Tone 1
+    Color(0xFF587EA4), // Muted Cool Tone 2
+    Color(0xFF6D8C97), // Neutralized Tone
   ];
 }
