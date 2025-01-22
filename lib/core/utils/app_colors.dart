3b124a9b8ae461767c8c3bce403558a5691ae278
hamdy haggag <hamdyhaggag74@gmail.com>
2025-01-23 00:12:37 +0200

    Add total amount display in the same row as the date and icon in the reports list

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -8,16 +8,16 @@ abstract class AppColors {
   static Color greyColor = Colors.grey;
   static const customColors = [
     Color(0xFF0961F5), // Primary Color
-    Color(0xFF3381F7), // 90% Tint
     Color(0xFF66A2F9), // 80% Tint
     Color(0xFF99C2FB), // 70% Tint
-    Color(0xFFCCE3FD), // 60% Tint
+    Color(0xFF3381F7), // 90% Tint
     Color(0xFF084CD3), // 10% Shade
     Color(0xFF063EAF), // 20% Shade
     Color(0xFF052F8B), // 30% Shade
-    Color(0xFF041F66), // 40% Shade
     Color(0xFF3979C8), // Muted Cool Tone 1
     Color(0xFF587EA4), // Muted Cool Tone 2
     Color(0xFF6D8C97), // Neutralized Tone
+    Color(0xFF041F66), // 40% Shade
+    Color(0xFFCCE3FD), // 60% Tint
   ];
 }
--- lib/features/reports/reports.dart
+++ lib/features/reports/reports.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:ramadan_kitchen_management/features/daily_expenses/services/expense_service.dart';
 
+import '../../core/utils/app_colors.dart';
+
 String formatDateString(String dateString) {
   try {
     final parsedDate = DateTime.parse(dateString);
-    return DateFormat('EEEE, MMM d, yyyy', 'ar').format(parsedDate);
+    return DateFormat('EEEE , d MMM yyyy', 'ar').format(parsedDate);
   } catch (e) {
     return 'تاريخ غير صالح';
   }
@@ -13,7 +15,7 @@ String formatDateString(String dateString) {
 
 String formatTimeString(String timeString) {
   try {
-    if (timeString.isEmpty) return 'وقت غير صالح';
+    if (timeString.isEmpty) return '';
 
     final now = DateTime.now();
     final timeParts = timeString.split(':');
@@ -28,7 +30,7 @@ String formatTimeString(String timeString) {
     final parsedTime = DateTime.parse(timeString);
     return DateFormat('h:mm a', 'ar').format(parsedTime);
   } catch (e) {
-    return 'وقت غير صالح';
+    return '';
   }
 }
 
@@ -43,79 +45,145 @@ class ReportsScreen extends StatelessWidget {
       future: _expenseService.loadExpenses(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
         }
 
         final groupedExpenses = _expenseService.getGroupedExpensesByDate();
 
         if (groupedExpenses.isEmpty) {
-          return const Center(
-            child: Text(
-              'لا توجد مصروفات مسجلة',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          return const Scaffold(
+            body: Center(
+              child: Text(
+                'لا توجد مصروفات مسجلة',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
             ),
           );
         }
 
         return Scaffold(
           body: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: ListView.builder(
               itemCount: groupedExpenses.length,
               itemBuilder: (context, index) {
                 final date = groupedExpenses.keys.elementAt(index);
                 final dailyExpenses = groupedExpenses[date]!;
 
+                double totalAmount = 0.0;
+                for (var expense in dailyExpenses) {
+                  totalAmount += expense['amount'] ?? 0.0;
+                }
+
                 return Card(
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
+                    borderRadius: BorderRadius.circular(15),
                   ),
-                  elevation: 6,
+                  elevation: 4,
                   margin: const EdgeInsets.symmetric(vertical: 8),
-                  shadowColor: Colors.black.withValues(alpha: 0.1),
-                  child: ExpansionTile(
-                    tilePadding:
-                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    title: Text(
-                      formatDateString(date),
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.teal[700],
-                      ),
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.calendar_today,
+                              color: AppColors.primaryColor.withAlpha(200),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              formatDateString(date),
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.blackColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        // Add total amount to the right of the row
+                        Text(
+                          '${totalAmount.toStringAsFixed(2)} ج.م',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                      ],
                     ),
                     subtitle: Text(
                       '${dailyExpenses.length} مصروفات',
                       style: TextStyle(fontSize: 14, color: Colors.grey[600]),
                     ),
-                    children: dailyExpenses.map((expense) {
-                      return ListTile(
-                        title: Text(
-                          expense['description'],
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w600,
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(20),
                           ),
                         ),
-                        subtitle: Text(
-                          'المبلغ: ${expense['amount']} ج.م',
-                          style:
-                              TextStyle(fontSize: 13, color: Colors.grey[700]),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.teal[50],
-                          child: Icon(
-                            Icons.attach_money,
-                            color: Colors.teal[400],
-                          ),
-                        ),
-                        trailing: Text(
-                          formatTimeString(expense['time'] ?? ''),
-                          style:
-                              TextStyle(fontSize: 13, color: Colors.grey[500]),
-                        ),
+                        builder: (_) {
+                          return Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  formatDateString(date),
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+                                ...dailyExpenses.map((expense) {
+                                  return ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    title: Text(
+                                      expense['description'],
+                                      style: const TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    subtitle: Text(
+                                      'المبلغ: ${expense['amount']} ج.م',
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.grey[700],
+                                      ),
+                                    ),
+                                    leading: CircleAvatar(
+                                      radius: 20,
+                                      backgroundColor:
+                                          AppColors.primaryColor.withAlpha(200),
+                                      child: Icon(
+                                        Icons.attach_money,
+                                        color: AppColors.whiteColor,
+                                      ),
+                                    ),
+                                    trailing: Text(
+                                      formatTimeString(expense['time'] ?? ''),
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Colors.grey[500],
+                                      ),
+                                    ),
+                                  );
+                                }).toList(),
+                              ],
+                            ),
+                          );
+                        },
                       );
-                    }).toList(),
+                    },
                   ),
                 );
               },
