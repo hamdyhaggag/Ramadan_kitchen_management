ec46f446e2a7928b9f96892c88508f948b12aba8
hamdy haggag <hamdyhaggag74@gmail.com>
2025-02-05 01:07:32 +0200

    Updated `MealDescription` with styled `Card` for better UI consistency.

--- lib/features/donation/presentation/views/widgets/meal_description.dart
+++ lib/features/donation/presentation/views/widgets/meal_description.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/app_colors.dart';
 
 class MealDescription extends StatelessWidget {
   final String description;
 
-  const MealDescription({required this.description});
+  const MealDescription({super.key, required this.description});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      description,
-      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            color: Colors.grey[600],
-            height: 1.6,
-          ),
+    return Card(
+      color: AppColors.primaryColor.withValues(alpha: 0.1),
+      elevation: 0,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+        side: BorderSide(
+          color: AppColors.whiteColor,
+          width: 1,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Text(
+          description,
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                color: AppColors.blackColor,
+                height: 1.6,
+              ),
+        ),
+      ),
     );
   }
 }
